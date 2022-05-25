165af7041b3335f6f4e358e022080757718c5547
Marius Rasbici <marius.rasbici@icloud.com>
2022-05-25 18:42:38 +0300

    Project depedencies files

---
+++ src/main/java/ro/itschool/managementappp/ManagementapppApplication.java
@@ -0,0 +1,13 @@
+package ro.itschool.managementappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementapppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementapppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/itschool/managementappp/ManagementapppApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.itschool.managementappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementapppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
